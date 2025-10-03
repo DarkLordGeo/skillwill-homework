0a39682c7943d0e1ea127071f62cc5ba9e6c2803
DarkLordGeo <148902362+DarkLordGeo@users.noreply.github.com>
2025-10-03 17:00:21 +0400

    10/3/2025

---
+++ _5/_5.js
@@ -0,0 +1,40 @@
+document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#hide").classList.toggle("hide")
+})
+
+
+
+
+const cardDiv = document.createElement('div')
+cardDiv.setAttribute("id", "card")
+
+const h2Element = document.createElement("h2")
+h2Element.textContent = "Gandalf"
+
+const hrefElement = document.createElement("a")
+hrefElement.setAttribute("href", "#")
+hrefElement.textContent = "Go to profile"
+
+
+cardDiv.appendChild(h2Element)
+cardDiv.appendChild(hrefElement)
+
+document.body.appendChild(cardDiv)
+// const html = `
+//     <div id="card">
+//         <h2>Gandalf</h2>
+//         <a href="#">Go to profile</a>
+//     </div>
+// `
+// document.querySelector("#i").innerHTML = html
+const buttons = document.querySelectorAll("button")
+buttons.forEach((answer) => {
+    answer.addEventListener("click", () => {
+        return answer.id == "answer_correct" ?
+            (
+                answer.style.color = 'green',
+                document.querySelector("#score").innerHTML++,
+                answer.setAttribute("disabled", true))
+            : answer.style.color = 'red'
+    })
+})
